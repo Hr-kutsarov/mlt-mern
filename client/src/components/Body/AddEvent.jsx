8c5f9ccd7bf587ad76572966c76adf648716f449
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-05-26 18:20:35 +0300

    Created form to add events via mutations.

--- client/src/components/Body/AddEvent.jsx
+++ client/src/components/Body/AddEvent.jsx
@@ -3,22 +3,33 @@ import { ADD_EVENT } from '../mutations/eventMutations.js'
 import { GET_EVENTS } from '../queries/eventQueries.js';
 import { useMutation } from '@apollo/client';
 import { useState } from 'react';
-import { Link } from 'react-router-dom'
 
 
 
 export function AddEvent() {
-    const [title, setTitle] = useState('test title')
-    const [summary, setSummary] = useState('test summary')
-    const [content, setContent] = useState('test content')
+    const [title, setTitle] = useState('')
+    const [summary, setSummary] = useState('')
+    const [content, setContent] = useState('')
 
     const [addEvent] = useMutation(ADD_EVENT, {
         variables: { title, summary, content },
         refetchQueries: [{ query: GET_EVENTS}]
     }) 
     return (
-        <>
-            <button onClick={addEvent}>Add test event</button>
-        </>
-    )
-}
+        <div id='add-event-form-wrapper'>
+            <span id="add-form-picture"></span>
+            <form id="add-event-form" onSubmit={addEvent}>
+                <h3>Create new event</h3>
+                <label>Title</label>
+                <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
+                <label>Summary</label>
+                <input type="text" onChange={(e) => setSummary(e.target.value)} value={summary}/>
+                <label>Content</label>
+                <input type="text" onChange={(e) => setContent(e.target.value)} value={content}/>
+                <button>Create</button>
+            </form>
+            
+        </div>
+ );
+ 
+ }
