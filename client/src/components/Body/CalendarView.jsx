5f59aed7195bc75c5ff4ef4b4215e483f4b8eed6
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-02 16:23:01 +0300

    New plugins

--- client/src/components/Body/CalendarView.jsx
+++ client/src/components/Body/CalendarView.jsx
@@ -2,6 +2,8 @@ import './CalendarView.css'
 
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list'
 import { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_ALL_DEVLOGS } from '../queries/devlogQueries.js'
@@ -10,8 +12,11 @@ export function CalendarView() {
     const [arr, setArr] = useState([])
     const { loading, error, data } = useQuery(GET_ALL_DEVLOGS, {
         onCompleted: (data) => {
+            
+            
             let result = []
             for (let el of data.getAllDevlogs) {
+                // setArr([...arr, el])
                 result.push({id: el._id,title: el.title, date: el.created})
             }
             setArr(result)
@@ -21,17 +26,21 @@ export function CalendarView() {
     return (
         <>
             <div id='calendar-view-wrapper'>
-                <section>
+                <section id='Calendar'>
                     <FullCalendar 
                     plugins={[ dayGridPlugin ]}
                     initialView="dayGridMonth"
+                    // removes the columns for Sat and Sun
                     // weekends={false}
-                      events={arr}
+                    events={arr}
                     />
                 </section>
                 <section>
                     <article>
-
+                        {/* just to check how this will look like */}
+                        <h1>Text</h1>
+                        <p>Text</p>
+                        <p>Text</p>
                     </article>
                 </section>
             </div>
