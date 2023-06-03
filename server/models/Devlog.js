e9e5eaf0ccd78628b12391f2493028312bfcc100
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-03 16:08:07 +0300

    Mongoose schema update. Timestamps and required.

--- server/models/Devlog.js
+++ server/models/Devlog.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const DevlogEntrySchema = new mongoose.Schema({
-    title: {type: String},
-    created: {type: String},
-    entry: {type: String}
-})
+    title: {type: String, required: true},
+    date: {type: String, required: true},
+    entry: {type: String, required: true}
+}, { timestamps: true})
 
-module.exports = mongoose.model('DevlogEntry', DevlogEntrySchema)
+
+module.exports = mongoose.model('Devlog', DevlogEntrySchema)
