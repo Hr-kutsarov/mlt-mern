888eecd831bbe2609a377e89837a484db97699e8
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-04 18:03:13 +0300

    Change server configuration.

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    const connection = await mongoose.connect(process.env.MONGO)
-    console.log(`MongoDB connected: ${connection.connection.host}`.blue.underline.bold)
-}
+// const connectDB = async () => {
+//     const connection = await mongoose.connect(process.env.MONGO)
+//     console.log(`MongoDB connected: ${connection.connection.host}`.blue.underline.bold)
+// }
 
-module.exports = connectDB;
+// module.exports = connectDB;
