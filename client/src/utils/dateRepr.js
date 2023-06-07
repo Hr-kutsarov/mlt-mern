b3cacec3657fe66bcb1eacb4249458228d4eaa38
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-07 10:59:17 +0300

    New utils.

---
+++ client/src/utils/dateRepr.js
@@ -0,0 +1,23 @@
+export const weekRepr = {
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
+    7: 'Sun'
+    }
+
+export const yearRepr = {
+    0: 'Jan',
+    1: 'Feb',
+    3: 'March',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec'
+}
