18a8c88b8986fca99e2cf1de7981ffdfd7f04c66
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-08 17:29:54 +0300

    Comment out unused mutations.

--- client/src/components/mutations/devlogMutations.js
+++ client/src/components/mutations/devlogMutations.js
@@ -1,39 +1,39 @@
-import { gql } from '@apollo/client'
+// import { gql } from '@apollo/client'
 
-const DELETE_DEVLOG = gql`
-    mutation deleteDevlog($id: ID!) {
-        deleteDevlog(id: $id) {
-            title
-            entry
-        }
-    }
-`
+// const DELETE_DEVLOG = gql`
+//     mutation deleteDevlog($id: ID!) {
+//         deleteDevlog(id: $id) {
+//             title
+//             entry
+//         }
+//     }
+// `
 
-const CREATE_DEVLOG = gql`
-    mutation addDevlog($title: String! $created: String! $entry: String!) {
-        addDevlog(title: $title, created: $created entry: $entry) {
-            title
-            entry
-        }
-    }
-`
+// const CREATE_DEVLOG = gql`
+//     mutation addDevlog($title: String! $created: String! $entry: String!) {
+//         addDevlog(title: $title, created: $created entry: $entry) {
+//             title
+//             entry
+//         }
+//     }
+// `
 
-const EDIT_DEVLOG = gql`
-    mutation editDevlog(
-        $id: ID!
-        $title: String!
-        $entry: String!
-        ) {
-            editDevlog(
-                id: $id,
-                title: $title,
-                entry: $entry
-                ) {
-                    _id
-                    title
-                }
-        }
-`
+// const EDIT_DEVLOG = gql`
+//     mutation editDevlog(
+//         $id: ID!
+//         $title: String!
+//         $entry: String!
+//         ) {
+//             editDevlog(
+//                 id: $id,
+//                 title: $title,
+//                 entry: $entry
+//                 ) {
+//                     _id
+//                     title
+//                 }
+//         }
+// `
 
 
-export { CREATE_DEVLOG, EDIT_DEVLOG , DELETE_DEVLOG}
+// export { CREATE_DEVLOG, EDIT_DEVLOG , DELETE_DEVLOG}
--- client/src/components/mutations/eventMutations.js
+++ client/src/components/mutations/eventMutations.js
@@ -1,45 +1,45 @@
-import { gql } from '@apollo/client'
+// import { gql } from '@apollo/client'
 
-const DELETE_EVENT = gql`
-    mutation deleteEvent($id: ID!) {
-        deleteEvent(id: $id) {
-            title
-            summary
-        }
-    }
-`
+// const DELETE_EVENT = gql`
+//     mutation deleteEvent($id: ID!) {
+//         deleteEvent(id: $id) {
+//             title
+//             summary
+//         }
+//     }
+// `
 
-const ADD_EVENT = gql`
-    mutation addEvent($title: String!, $summary: String!, $content: String!, $pictureUrl: String! ) {
-        addEvent(title: $title, summary: $summary, content: $content, pictureUrl: $pictureUrl) {
-            title
-            summary
-            content
-            pictureUrl
-        }
-    }
-`
+// const ADD_EVENT = gql`
+//     mutation addEvent($title: String!, $summary: String!, $content: String!, $pictureUrl: String! ) {
+//         addEvent(title: $title, summary: $summary, content: $content, pictureUrl: $pictureUrl) {
+//             title
+//             summary
+//             content
+//             pictureUrl
+//         }
+//     }
+// `
 
-const EDIT_EVENT = gql`
-    mutation editEvent(
-        $id: ID!
-        $title: String!
-        $summary: String!
-        $content: String!
-        $pictureUrl: String!
-        ) {
-            editEvent(
-                id: $id,
-                title: $title
-                summary: $summary
-                content: $content
-                pictureUrl: $pictureUrl
-                ) {
-                    title
-                    pictureUrl
-                }
-        }
-`
+// const EDIT_EVENT = gql`
+//     mutation editEvent(
+//         $id: ID!
+//         $title: String!
+//         $summary: String!
+//         $content: String!
+//         $pictureUrl: String!
+//         ) {
+//             editEvent(
+//                 id: $id,
+//                 title: $title
+//                 summary: $summary
+//                 content: $content
+//                 pictureUrl: $pictureUrl
+//                 ) {
+//                     title
+//                     pictureUrl
+//                 }
+//         }
+// `
 
 
-export { DELETE_EVENT, ADD_EVENT , EDIT_EVENT}
+// export { DELETE_EVENT, ADD_EVENT , EDIT_EVENT}
