a9c9c5e38e4e8d3a7913ab545178b1c7b27b9390
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-12 21:58:31 +0300

    Display announcements.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,4 @@
+import { Announcement } from './Body/DevlogEntries/Announcement.jsx'
 import { EventReel } from './Body/EventReel/EventReel.jsx'
 import './Main.css'
 
@@ -6,6 +7,8 @@ export function Main() {
     return (
         <>
             <main>
+                <Announcement />
+                <h2>Watch now</h2>
                 <EventReel key="event-reel" />
             </main>
         </>
