853965e34eb38e3d607a7b6aae2981cb2d7eae5c
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-06-14 17:05:37 +0300

    Updated logo.

--- client/src/components/Header/LogoBox.jsx
+++ client/src/components/Header/LogoBox.jsx
@@ -6,6 +6,7 @@ export function LogoBox() {
             
                 <div id='logo-box'>
                     <span id='company-logo-image'></span>
+                    <h2>My Local Theatre</h2>
                 </div>
             
         </>
