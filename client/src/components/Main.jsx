4b6d207ab731003ccab18a14772a8c9a162154a8
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-03 17:46:06 +0300

    Improved main section overall look.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,14 +1,16 @@
 import { ArtistsSlider } from './Body/Artists/ArtistsSlider.jsx'
 import { Announcement } from './Body/DevlogEntries/Announcement.jsx'
 import { EventReel } from './Body/EventReel/EventReel.jsx'
+import { HeroSection } from './Body/Hero/Hero.jsx'
 import './Main.css'
 
 export function Main() {
     
     return (
         <main>
-            <Announcement />
+            <HeroSection />
             <EventReel />
+            <Announcement />
             <ArtistsSlider />
         </main>
     )}
