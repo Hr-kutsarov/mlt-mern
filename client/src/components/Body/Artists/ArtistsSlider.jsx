6d433e9c00e653dd10f495633064a49460c33c3c
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-05 16:16:36 +0300

    Artists are now stored in Global state.

--- client/src/components/Body/Artists/ArtistsSlider.jsx
+++ client/src/components/Body/Artists/ArtistsSlider.jsx
@@ -5,16 +5,18 @@ import { motion } from 'framer-motion'
 import './ArtistsSlider.css'
 import { FaStar } from "react-icons/fa"
 import { Loading } from "../EventReel/Loading"
+import { useArtistStore } from "../../../store/appStore"
 
 export function ArtistsSlider() {
-    const [data, setData] = useState([])
     const [err, setErr] = useState('')
     const [loading, setLoading] = useState(true)
+    const setArtists = useArtistStore((state) => state.setArtists)
+    const artistsArray = useArtistStore((state) => state.artistsArray)
 
     const getArtists = async () => {
         api.get('/artists')
             .then((res) => {
-                setData(res.data)
+                setArtists(res.data)
             })
             .catch((err) => {
                 setErr(err.message)
@@ -25,7 +27,7 @@ export function ArtistsSlider() {
     }
 
     const maxItemsOnScreen = 4
-    const maxLeftValue = (350 * Number(data.length - maxItemsOnScreen))
+    const maxLeftValue = (350 * Number(artistsArray.length - maxItemsOnScreen))
     
     useEffect(() => {
         getArtists()
@@ -36,13 +38,13 @@ export function ArtistsSlider() {
         <>
         {loading && !err && (<Loading />)}
         {!loading && err && (<h2>{err}</h2>)}
-        {!loading && !err && data && (
+        {!loading && !err && artistsArray && (
             <>
             <h2>MEET US</h2>
             <h3><FaStar /><FaStar /><FaStar /></h3>
             <motion.div id="carousel">
                 <motion.div drag="x" dragConstraints={{right: -20, left: -maxLeftValue}} id="inner-carousel">
-                    {data && (data.map((artist) => (<ArtistSlide key={artist._id} artist={artist}/>)))}
+                    {artistsArray && (artistsArray.map((artist) => (<ArtistSlide key={artist._id} artist={artist}/>)))}
                 </motion.div>
             </motion.div>
             </>
