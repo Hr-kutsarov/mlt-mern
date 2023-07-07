d426258a06030c3c5e8849ef3ed6059057c6723f
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-07 21:58:46 +0300

    Implemented logic for selecting artists

--- client/src/components/Body/EventReel/SelectedArtist.jsx
+++ client/src/components/Body/EventReel/SelectedArtist.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react"
 import "./SelectedArtist.css"
+import { useArtistStore } from "../../../store/appStore"
 
 export function SelectedArtist({artist}) {
 
     const [ selected, setSelected ] = useState(false)
+    const selectedArtists = useArtistStore((state) => state.selectedArtists)
+    const setSelectedArtists = useArtistStore((state) => state.setSelectedArtists)
 
-    const toggleSelect = () => {
-        setSelected(!selected)
+
+    const toggleSelect = (e) => {
+        setSelected(true)
+        if (!selected) {
+            setSelectedArtists([...selectedArtists, artist])
+        } else if (selected) {
+            return
+        }        
     }
+
     return (
         <span id="selected-artist-wrapper" onClick={toggleSelect} style={selected ? {opacity: 0.5} : {opacity: 1}}>
             <img src={artist.photo} alt={artist.name}/>
+            <span>
+                <p>
+                    {artist.name}
+                </p>
+            </span>
         </span>
     )
 }
