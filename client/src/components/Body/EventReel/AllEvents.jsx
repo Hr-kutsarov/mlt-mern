9c684ec9213fa33575376d82ed39aa5594b2526e
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-11 17:40:49 +0300

    Added AllEvents component.

---
+++ client/src/components/Body/EventReel/AllEvents.jsx
@@ -0,0 +1,13 @@
+import { EventReel } from "./EventReel";
+import { Header } from "../../Header";
+import { Footer } from "../../Footer";
+
+export function AllEvents() {
+    return (
+    <main>
+        <Header />
+        <EventReel />
+        <Footer />
+    </main>
+    )
+}
