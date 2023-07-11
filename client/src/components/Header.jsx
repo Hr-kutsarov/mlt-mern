0a240e46bc3f9ad7e9f94b2caa5a5e78dd7001d5
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-11 18:47:28 +0300

    Header navigation list added to modal.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,23 @@
 import './Header.css';
 import { LogoBox } from './Header/LogoBox'
 import { Navigation } from './Header/Navigation'
+import { IoIosMenu } from 'react-icons/io'
+import { useState } from 'react';
 
 export function Header() {
+    const [open, setOpen] = useState(false)
+
+    const toggleNavigation = () => {
+        setOpen(!open)
+    }
+
     return (
         <>
             <header>
                 <LogoBox />
-                <Navigation />
+                <IoIosMenu onClick={toggleNavigation}/>
             </header>
+            {open && (<Navigation toggleNavigation={toggleNavigation}/>)}
         </>
     )
 }
