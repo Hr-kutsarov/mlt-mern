43193339db2da9fce627e86d971384ef8adff036
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-12 18:14:19 +0300

    Rewrote Profile component render logic.

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -44,38 +44,62 @@ export function Profile() {
 
     }, [])
     return (
-        <span id='profile-wrapper'>
-            {userId ? (<>
-                     <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/events">Events</Link></li>
-                    <li>
-                        <span>
-                            <FaUser />
-                        </span>
-                    </li>
-                    <li>Hi, {username}</li>
-                    <li><Link to="/logout">Logout</Link></li>
-                </ul>
-            </nav>
-            <section id="permissions-info">
-                <h5><FaUserAlt /> You are viewing this website as {currentRole} {err}</h5>
-                <button onClick={handleSwitchRole}>Switch</button>
-            </section>
-            <section id="tickets-list">
-                {tickets ? (tickets.map((ticket) => (<Ticket key={ticket._id} ticket={ticket}/>))) : (<h1>You have not purchased any tickets yet.</h1>)}
-            </section>
-            </>) : (
-                <>
+        <>
+        <span id="profile-wrapper">
+                {!userId && (
                     <section id='profile-error'>
                         <h1>You must be logged in to visit this page.</h1>
                         <h3><Link to='/'>Navigate to homepage.</Link></h3>
                         <h4><Link to='/login'>Proceed to login.</Link></h4>
                         <h5><Link to='/register'>New around here? You can register at this page.</Link></h5>
                     </section>
-                </>
-            )}
+                )}
+                {userId && (role !== "moderator") && (
+                    <>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/events">Events</Link></li>
+                            <li>
+                                <span>
+                                    <FaUser />
+                                </span>
+                            </li>
+                            <li>Hi, {username}</li>
+                            <li><Link to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                    </>
+                )}
+                {userId && (role === "moderator") && (
+                    <>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/events">Events</Link></li>
+                            <li>
+                                <span>
+                                    <FaUser />
+                                </span>
+                            </li>
+                            <li><Link to="/admin-section">Admin</Link></li>
+                            <li><Link to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                    </>
+                )}
+                {userId && (
+                    <>
+                    <section id="permissions-info">
+                        <h5><FaUserAlt /> You are viewing this website as {currentRole} {err}</h5>
+                        <button onClick={handleSwitchRole}>Switch</button>
+                    </section>
+                    <section id="tickets-list">
+                        {tickets ? (tickets.map((ticket) => (<Ticket key={ticket._id} ticket={ticket}/>))) : (<h1>You have not purchased any tickets yet.</h1>)}
+                    </section>
+                    </>
+                )}
         </span>
+        </>
     )
 }
