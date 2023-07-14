04deebfffccc00fe24e69da7749cc0ef7e5249c6
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-14 19:45:51 +0300

    Created income section.

---
+++ client/src/components/Body/AdminSection/IncomeSection.jsx
@@ -0,0 +1,25 @@
+import './IncomeSection.css'
+import { useReducer } from 'react'
+import { Link } from 'react-router-dom'
+
+export function IncomeSection({ticketData}) {
+    
+    const calcTotal = () => {
+        let arr = ticketData.map((ticket) => ticket.price)
+        const sum = arr.reduce((acc, cv) => acc + cv, 0)
+        return sum
+    }
+
+    return (
+        <span id="income-section-wrapper">
+            <span>
+                <h1>Tickets</h1>
+                <h4>Sold Total: {ticketData.length}</h4>
+            </span>
+            <span>
+                <h1>Income </h1>
+                <h4>Total: ${calcTotal()}</h4>
+            </span>
+        </span>
+    )
+}
