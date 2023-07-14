d9386e9ef538eedd0c4388de17102e7e58314f60
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-14 10:13:40 +0300

    Logo box code cleaup.

--- client/src/components/Header/LogoBox.jsx
+++ client/src/components/Header/LogoBox.jsx
@@ -2,14 +2,10 @@ import './LogoBox.css'
 
 export function LogoBox() {
     return (
-        <>
-            
-                <div id='logo-box'>
-                    <h2>My Local</h2>
-                    <span id='company-logo-image'></span>
-                    <h2>Theatre</h2>
-                </div>
-            
-        </>
+        <span id='logo-box'>
+            <h2>My Local</h2>
+            <span id='company-logo-image'></span>
+            <h2>Theatre</h2>
+        </span>
     )
 }
