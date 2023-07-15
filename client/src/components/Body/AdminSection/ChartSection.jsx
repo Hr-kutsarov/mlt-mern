0e19252234451c2c576a8782c680345cc3a55718
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2023-07-15 15:19:22 +0300

    Completed chart section.

--- client/src/components/Body/AdminSection/ChartSection.jsx
+++ client/src/components/Body/AdminSection/ChartSection.jsx
@@ -1,93 +1,91 @@
 import './ChartSection.css'
-import { Link } from 'react-router-dom'
-import Chart from "react-apexcharts";
-import { useEffect } from 'react';
-import { Try } from './Try';
+import { PieChart } from './PieChart';
+import { BarChart } from './BarChart';
 
 export function ChartSection({ticketData}) {
 
-    const profitArray = () => {
-        let buff = {};
-        let arr = []
-        ticketData.forEach((t) => {
-            if (buff.hasOwnProperty(t.title)) {
-                buff[t.title] = buff[t.title] + t.price
-            } else {
-                buff[t.title] = t.price
-            }
-        })
+  const profitArray = () => {
+      let buff = {};
+      let arr = []
+      ticketData.forEach((t) => {
+          if (buff.hasOwnProperty(t.title)) {
+              buff[t.title] = buff[t.title] + t.price
+          } else {
+              buff[t.title] = t.price
+          }
+      })
+      for (let b in buff) {
+        arr.push({ id: b, label: b, value: buff[b].toFixed(2)})
+      }
 
-        for (let prop in buff) {
-            arr.push({ id: prop , label: buff[prop], value: buff[prop]})
-        }
+      return arr
+  }
 
-        return arr.map((a) => a.value)
+  const data = profitArray()
+
+
+
+
+  let barData = [
+    {
+      "weekday": "MON",
+    },
+    {
+      "weekday": "TUE",
+    },
+    {
+      "weekday": "WED",
+    },
+    {
+      "weekday": "THU",
+    },
+    {
+      "weekday": "FRI",
+    },
+    {
+      "weekday": "SAT",
+    },
+    {
+      "weekday": "SUN",
     }
+  ]
 
-    const pieOptions = {
-      options: {
-      },
-      series: [12, 23],
-      labels: ['asd', 'aa', 'aaa'],
+  const findObjByDay = (strData) => {
+    let str = strData.toUpperCase()
+    console.log(str)
+    for (let i=0; i<barData.length; i++) {
+      if (barData[i]['weekday'] === str) {
+        return barData[i]
+      }
     }
+  }
 
-    const ss = {
-      options: {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
-      },
-      series: [
-        {
-          name: ['asd'],
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
-    };
+  const dataSetter = () => {
+    ticketData.forEach((ticket) => {
+      // console.log(ticket['date'].split(' ')[0])
+      let ticketDay = ticket['date'].split(' ')[0]
+      let foundObj = findObjByDay(ticketDay)
+      if (foundObj[ticket['title']] === undefined) {
+        foundObj[ticket['title']] = 1
+      } else {
+        foundObj[ticket['title']] += 1
+      }
+    })
+    return barData
+  }
 
+  const dataBar = dataSetter()
 
-    // const data = [
-    //     {
-    //         "id": "scala",
-    //         "label": "scala",
-    //         "value": 434,
-    //         "color": "hsl(321, 70%, 50%)"
-    //     },
-    //     {
-    //         "id": "java",
-    //         "label": "java",
-    //         "value": 268,
-    //         "color": "hsl(304, 70%, 50%)"
-    //     },
-    //     {
-    //         "id": "php",
-    //         "label": "php",
-    //         "value": 225,
-    //         "color": "hsl(102, 70%, 50%)"
-    //     },
-    //     {
-    //         "id": "css",
-    //         "label": "css",
-    //         "value": 370,
-    //         "color": "hsl(292, 70%, 50%)"
-    //     },
-    // ]
+  const keys = data.map((a) => a.id)
 
-    const data = profitArray()
-    // useEffect(() => {
-    //     asd()
-    // })
 
     return (
         <span id="chart-section-wrapper">
-            <span className='donut'>
-                <Chart options={pieOptions.options} series={pieOptions.series} type="donut" width="380" />
+            <span>
+                <BarChart data={dataBar} dataKeys={keys}/>
             </span>
             <span>
-                <Try data={data}/>
+                <PieChart dataPie={data}/>
             </span>
         </span>
     )
